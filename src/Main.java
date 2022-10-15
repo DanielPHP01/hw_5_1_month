8c6b7fdb527e0fe815f6600056365b0ef2e784c0
DanielPHP01 <danielmokushov01@gmail.com>
2022-10-16 00:07:00 +0600

    Custom version for addition HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -8,29 +6,24 @@ public class Main {
         boss.setDamage(50);
         boss.setDefence("SpiderMan");
         System.out.println("Health " + boss.getHealth() + " Damage " + boss.getDamage() + " Defence " + boss.getDefence());
-
-
+        for (int i = 0; i < createHero().length; i++) {
+            System.out.println(" Hero health  = " + createHero()[i].getHealth() + " Damage = " + createHero()[i].getDamage() + " SuperSkill " + createHero()[i].getSuperSkill());
+        }
 
 
     }
 // не смог вернуть результат метода
 
-    public static Hero createHeroes(Hero[] heroes) {
 
-        Hero capitanAmerica = new Hero(250,30,"shield");
-        Hero spiderMan = new Hero(150, 25, "ShotWeb");
+    public static Hero[] createHero() {
+        Hero capitanAmerica = new Hero(250, 30, "Shield");
+        Hero spiderMan = new Hero(150, 25, "Shotweb");
         Hero doctorStrange = new Hero(200, 80, "Magical");
 
-
-        for (Hero hero : heroes) {
-            System.out.println(hero.getHealth() + hero.getDamage() + hero.getSuperSkill());
-            return hero;
-        } return createHeroes(heroes);
-
-
-
-
-
-
+        Hero[] showHeroes = {capitanAmerica, spiderMan, doctorStrange};
+        return showHeroes;
     }
 }
+
+
+
